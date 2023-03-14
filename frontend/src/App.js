29c3a6715cfb78c9c8bc643e21926e2b281152c4
Sachinda <sachinda46@gmail.com>
2023-03-14 18:25:09 +0530

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ClothingStore</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
